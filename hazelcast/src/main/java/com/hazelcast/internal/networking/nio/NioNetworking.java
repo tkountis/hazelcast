1ecf8d2dd7aecfc78fb0fcdb84b500b52c874548
Peter Veentjer <alarmnummer@gmail.com>
2020-01-09 16:07:07 +0200

    WIP

--- hazelcast/src/main/java/com/hazelcast/internal/networking/nio/NioNetworking.java
+++ hazelcast/src/main/java/com/hazelcast/internal/networking/nio/NioNetworking.java
@@ -134,7 +134,9 @@ public final class NioNetworking implements Networking, DynamicMetricsProvider {
         this.metricsRegistry = ctx.metricsRegistry;
         this.loggingService = ctx.loggingService;
         this.inputThreadCount = ctx.inputThreadCount;
+        this.activeInputThreads.set(inputThreadCount);
         this.outputThreadCount = ctx.outputThreadCount;
+        this.activeOutputThreads.set(outputThreadCount);
         this.logger = loggingService.getLogger(NioNetworking.class);
         this.errorHandler = ctx.errorHandler;
         this.balancerIntervalSeconds = ctx.balancerIntervalSeconds;
